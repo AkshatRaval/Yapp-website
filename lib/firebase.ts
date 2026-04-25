b59fce1d6e09a857cc271a00c0c79b1764730a5e
AkshatRaval <akshatraval199@gmail.com>
2026-04-25 23:24:24 +0530

    env set

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWYP9M4WchzMmCMn36qppHd4aXsj5TFxk",
-  authDomain: "ruracare-b069a.firebaseapp.com",
-  projectId: "ruracare-b069a",
-  storageBucket: "ruracare-b069a.firebasestorage.app",
-  messagingSenderId: "798156184383",
-  appId: "1:798156184383:web:1be362cd971ace09d4918a",
-  measurementId: "G-D8VK2NT74V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
